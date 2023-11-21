c6108cd1af41507100eab1b98353272ecb4556d1
Tetsushi Saburi <tetsushi.saburi.j7a@jp.denso.com>
2023-11-21 15:31:38 +0900

    feat: タブーサーチを追加

---
+++ src/io.jl
@@ -0,0 +1,27 @@
+function show_board(sol::Array{Int})::Nothing
+    n = length(sol)
+    for i = 1:n
+        for j = 1:n
+            if sol[i] == j
+                print("o ")
+            else
+                print(". ")
+            end
+        end
+        println()
+    end
+end
+
+function show_log(
+    sol::Array{Int},
+    diag_up::Array{Int},
+    diag_dn::Array{Int},
+)
+    println("array: ", sol)
+    show_board(sol)
+    println("queens on upward diagonals: ", diag_up)
+    println("queens on downward diagonals: ", diag_dn)
+    n_colls = collisions(diag_up) + collisions(diag_dn)
+    println("#collisions: ", n_colls)
+    println()
+end
---
+++ src/util.jl
@@ -0,0 +1,9 @@
+struct TimeKeeper
+    start_time_::DateTime
+    time_threshold_seconds_::Float16
+end
+
+@inline function isTimeOver(time_keeper::TimeKeeper)
+    diff = now() - time_keeper.start_time_  # milliseconds
+    return diff.value >= time_keeper.time_threshold_seconds_ * 1000
+end
